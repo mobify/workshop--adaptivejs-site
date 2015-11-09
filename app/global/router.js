6c7f5b4e309ced5bbdd7a6962261400c6551ac26
Stewart Yu <stewartyu@gmail.com>
2015-11-09 10:43:01 -0800

    remove category files from step 1

--- app/global/router.js
+++ app/global/router.js
@@ -1,15 +1,13 @@
 define([
     '$',
     'adaptivejs/router',
-    'pages/home/view',
-    'pages/home/category'
+    'pages/home/view'
 ],
 function($, Router, Home, Category) {
     var router = new Router();
 
     router
         .add(Router.selectorMatch('body.home'), Home)
-        .add(Router.selectorMatch('body.category'), Category)
         .add(Router.urlMatch('/foo'), Home)
         .add(function() {return true;}, Home);
 
